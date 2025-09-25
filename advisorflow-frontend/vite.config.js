23deb1c65293e0cffc2708ef34cc6276d0f012a4
Nyembz <nyembezimaduna2@gmail.com>
2025-09-25 14:49:39 +0200

    Initial commit

--- advisorflow-frontend/vite.config.js
+++ advisorflow-frontend/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL } from 'node:url'
 
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
